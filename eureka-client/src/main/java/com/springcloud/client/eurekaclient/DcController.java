90fa130514f809e8243f6dcf19e51a788690feef
luxin212121 <917923756@qq.com>
2018-12-14 15:06:13 +0800

    aa

---
+++ eureka-client/src/main/java/com/springcloud/client/eurekaclient/DcController.java
@@ -0,0 +1,23 @@
+package com.springcloud.client.eurekaclient;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class DcController {
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @RequestMapping("/dc")
+    public String index() throws InterruptedException {
+        Thread.sleep(5000L);
+        String services ="server: " + discoveryClient.getServices();
+        return services;
+    }
+
+}
---
+++ eureka-consume/src/main/java/com/springcloud/eurekaconsume/DcController.java
@@ -0,0 +1,42 @@
+package com.springcloud.eurekaconsume;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@EnableDiscoveryClient
+@RestController
+public class DcController {
+
+    @Autowired
+    LoadBalancerClient loadBalancerClient;
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @Autowired
+    ConsumerService consumerService;
+
+    @GetMapping("/consumer")
+    public String dc() {
+        ServiceInstance serviceInstance = loadBalancerClient.choose("eureka-client");
+        String url = "http://" + serviceInstance.getHost() + ":" + serviceInstance.getPort() + "/dc";
+        return restTemplate.getForObject(url, String.class);
+    }
+
+    @GetMapping("/consumerR")
+    public String dcRibbon(){
+        return restTemplate.getForObject("http://eureka-client/dc",String.class);
+    }
+
+    @GetMapping("/consumerHy")
+    public String hyDc() {
+        return consumerService.consumer();
+    }
+
+
+}
